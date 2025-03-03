beb7228ee8c776e2269f0b700a34bc14a39fdf78
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-03 16:33:38 +0700

    Commit changes

--- OperatorAritmatika.php
+++ OperatorAritmatika.php
@@ -1,23 +1,46 @@
 <?php
-//Operator Aritmatika (1)
+// Operator Aritmatika (1)
 // +$variable Positif
 // -$variable Negatif
 // $ + $ Penambahan
 // $ - $ Pengurangan
 // $ * $ Perkalian
-// $variable / $variable pembagian
+// $ / $ Pembagian
+// $ % $ Sisa Bagi
+// $ ** $ Pangkat
 
 $a = 10;
 $b = 10;
 
+// Penambahan
 $result = $a + $b;
-var_dump($result);
+var_dump($result); // int(20)
 
+// Negatif
 $resultNegative = -$result;
-var_dump($resultNegative);
+var_dump($resultNegative); // int(-20)
 
+// Positif
 $resultPositive = +$result;
-var_dump($resultPositive);
+var_dump($resultPositive); // int(20)
 
+// Sisa Bagi
 $resultModulo = 100 % 3;
-var_dump($resultModulo);
+var_dump($resultModulo); // int(1)
+
+// Pengurangan
+$resultSubtraction = $a - $b;
+var_dump($resultSubtraction); // int(0)
+
+// Perkalian
+$resultMultiplication = $a * $b;
+var_dump($resultMultiplication); // int(100)
+
+// Pembagian
+$resultDivision = $a / $b;
+var_dump($resultDivision); // float(1)
+
+// Pangkat
+$resultExponentiation = $a ** 2;
+var_dump($resultExponentiation); // int(100)
+?>
