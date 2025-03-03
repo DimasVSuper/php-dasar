53c1e5d5cf42577fb4bf6cae094850ba41944d1a
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-03 16:50:14 +0700

    OperatorPerbandingan.php changes

--- OperatorPerbandingan.php
+++ OperatorPerbandingan.php
@@ -5,6 +5,10 @@
 // $ != $ Tidak sama dengan: True jika $a tidak sama dengan $b setelah dilakukan konversi tipe data
 // $ <> $ Tidak sama dengan: True jika $a tidak sama dengan $b setelah dilakukan konversi tipe data
 // $ !== $ Tidak identik: True jika $a tidak sama dengan $b atau tidak memiliki tipe data yang sama
+// $ < $ Lebih kecil dari: True jika $a lebih kecil dari $b
+// $ <= $ Lebih kecil atau sama dengan: True jika $a lebih kecil atau sama dengan $b
+// $ > $ Lebih besar dari: True jika $a lebih besar dari $b
+// $ >= $ Lebih besar atau sama dengan: True jika $a lebih besar atau sama dengan $b
 
 $a = "10";
 $b = 10;
@@ -29,4 +33,10 @@ var_dump(10 < 9); // False, karena 10 tidak lebih kecil dari 9
 
 // Lebih besar atau sama dengan (>=)
 var_dump(9 >= 9); // True, karena 9 sama dengan 9
+
+// Lebih kecil atau sama dengan (<=)
+var_dump(8 <= 10); // True, karena 8 lebih kecil dari 10
+
+// Lebih besar dari (>)
+var_dump(10 > 5); // True, karena 10 lebih besar dari 5
 ?>
