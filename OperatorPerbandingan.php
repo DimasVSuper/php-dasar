5a0b1a632ca5cfa313db9f2cf057282cc92831be
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-03 16:46:13 +0700

    Commit changes

--- OperatorPerbandingan.php
+++ OperatorPerbandingan.php
@@ -1,7 +1,32 @@
 <?php
+// Operator Perbandingan
+// $ == $ Sama dengan: True jika $a sama dengan $b setelah dilakukan konversi tipe data
+// $ === $ Identik: True jika $a sama dengan $b dan memiliki tipe data yang sama
+// $ != $ Tidak sama dengan: True jika $a tidak sama dengan $b setelah dilakukan konversi tipe data
+// $ <> $ Tidak sama dengan: True jika $a tidak sama dengan $b setelah dilakukan konversi tipe data
+// $ !== $ Tidak identik: True jika $a tidak sama dengan $b atau tidak memiliki tipe data yang sama
 
-var_dump("10" == 10);
-var_dump("10" === 10);
+$a = "10";
+$b = 10;
 
-var_dump(10 < 9);
-var_dump(9 >= 9);
+// Sama dengan (==)
+var_dump($a == $b); // True, karena "10" sama dengan 10 setelah konversi tipe data
+
+// Identik (===)
+var_dump($a === $b); // False, karena "10" (string) tidak identik dengan 10 (integer)
+
+// Tidak sama dengan (!=)
+var_dump($a != $b); // False, karena "10" sama dengan 10 setelah konversi tipe data
+
+// Tidak sama dengan (<>)
+var_dump($a <> $b); // False, karena "10" sama dengan 10 setelah konversi tipe data
+
+// Tidak identik (!==)
+var_dump($a !== $b); // True, karena "10" (string) tidak identik dengan 10 (integer)
+
+// Lebih kecil dari (<)
+var_dump(10 < 9); // False, karena 10 tidak lebih kecil dari 9
+
+// Lebih besar atau sama dengan (>=)
+var_dump(9 >= 9); // True, karena 9 sama dengan 9
+?>
