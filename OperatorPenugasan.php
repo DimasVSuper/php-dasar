3c5ba7c70d1682cae7f69b4b62a603bfe1af7056
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-03 16:39:17 +0700

    commit changes

--- OperatorPenugasan.php
+++ OperatorPenugasan.php
@@ -1,4 +1,10 @@
 <?php
+// Operator Penugasan Aritmatika
+// $ += $ 
+// $ -= $
+// $ *= $
+// $ /= $
+// $ %= $
 
 $total = 0;
 
@@ -6,9 +12,28 @@ $fruit = 5000;
 $chicken = 10000;
 $orangeJuice = 5000;
 
+// Menambahkan harga buah ke total
+$total += $fruit; // $total = $total + $fruit
+// Menambahkan harga ayam ke total
+$total += $chicken; // $total = $total + $chicken
+// Menambahkan harga jus jeruk ke total
+$total += $orangeJuice; // $total = $total + $orangeJuice
 
-$total += $fruit;
-$total += $chicken;
-$total += $orangeJuice;
+var_dump($total); // int(20000)
 
-var_dump($total);
+// Mengurangi harga buah dari total
+$total -= $fruit; // $total = $total - $fruit
+var_dump($total); // int(15000)
+
+// Mengalikan total dengan 2
+$total *= 2; // $total = $total * 2
+var_dump($total); // int(30000)
+
+// Membagi total dengan 3
+$total /= 3; // $total = $total / 3
+var_dump($total); // float(10000)
+
+// Menghitung sisa bagi total dengan 1000
+$total %= 1000; // $total = $total % 1000
+var_dump($total); // int(0)
+?>
