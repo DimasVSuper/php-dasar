b22ce8abe7a5e66f1ebd3604f42ab2aa14c7de77
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-04 20:06:07 +0700

    Commit changes

--- OperatorLogika.php
+++ OperatorLogika.php
@@ -1,7 +1,14 @@
 <?php
-// Operator Logika
-
+// Operator Logika dan nama
+// $a && $b  // And: Mengembalikan true jika kedua operand bernilai true
+// $a and $b // And: Sama seperti &&, tetapi dengan prioritas yang lebih rendah
+// $a || $b  // Or: Mengembalikan true jika salah satu operand bernilai true
+// $a or $b  // Or: Sama seperti ||, tetapi dengan prioritas yang lebih rendah
+// $a xor $b // Xor: Mengembalikan true jika salah satu operand bernilai true, tetapi tidak keduanya
+// !$a       // Not: Mengembalikan true jika operand bernilai false
 // AND (&&) - True jika kedua operand bernilai true
+
+
 var_dump(true && true);  // True, karena kedua operand bernilai true
 var_dump(true && false); // False, karena salah satu operand bernilai false
 
