93709ff91ea39e4da840893f1882e9bc71cf6991
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-04 20:09:55 +0700

    commit IncrementDanDecrement change

--- IncrementDanDecrement.php
+++ IncrementDanDecrement.php
@@ -1,8 +1,10 @@
 <?php
 
-$a = 10;
+$a = 10; // Inisialisasi variabel $a dengan nilai 10
 
-$b = ++$a;
+// Pre-increment: Menambah nilai $a terlebih dahulu, kemudian mengembalikan nilai $a
+$b = ++$a; // $a menjadi 11, kemudian nilai $a (11) diberikan ke $b
 
-var_dump($a);
-var_dump($b);
+var_dump($a); // Menampilkan nilai $a, yaitu 11
+var_dump($b); // Menampilkan nilai $b, yaitu 11
+?>
