c5b3ded8cd9db75d84ce3473a187fe4979823332
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-04 20:02:56 +0700

    Commit OperatorLogika changes

--- OperatorLogika.php
+++ OperatorLogika.php
@@ -1,13 +1,19 @@
 <?php
+// Operator Logika
 
-var_dump(true && true);
-var_dump(true && false);
+// AND (&&) - True jika kedua operand bernilai true
+var_dump(true && true);  // True, karena kedua operand bernilai true
+var_dump(true && false); // False, karena salah satu operand bernilai false
 
-var_dump(true || false);
-var_dump(true || true);
+// OR (||) - True jika salah satu operand bernilai true
+var_dump(true || false); // True, karena salah satu operand bernilai true
+var_dump(true || true);  // True, karena kedua operand bernilai true
 
-var_dump(true xor false);
-var_dump(true xor true);
+// XOR (xor) - True jika salah satu operand bernilai true, tetapi tidak keduanya
+var_dump(true xor false); // True, karena salah satu operand bernilai true
+var_dump(true xor true);  // False, karena kedua operand bernilai true
 
-var_dump(!true);
-var_dump(!false);
+// NOT (!) - Membalik nilai boolean operand
+var_dump(!true);  // False, karena operand bernilai true
+var_dump(!false); // True, karena operand bernilai false
+?>
