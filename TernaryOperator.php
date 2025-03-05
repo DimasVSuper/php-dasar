cc41919ff3856044a17923171ebde1bb94d467ed
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-05 20:38:57 +0700

    Commit TernaryOperator changes

--- TernaryOperator.php
+++ TernaryOperator.php
@@ -1,6 +1,13 @@
 <?php
 
+// Mendefinisikan variabel gender dengan nilai "PRIA"
 $gender = "PRIA";
+
+// Menggunakan operator ternary untuk menentukan nilai variabel hi
+// Jika gender adalah "PRIA", maka hi akan bernilai "Hi bro!"
+// Jika gender bukan "PRIA", maka hi akan bernilai "Hi nona!"
 $hi = $gender == "PRIA" ? "Hi bro!" : "Hi nona!";
 
+// Menampilkan nilai variabel hi diikuti dengan karakter akhir baris
 echo $hi . PHP_EOL;
+
