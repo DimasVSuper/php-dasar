f9b0963f80e1678baa639ce1f3770eab2995c0dd
Dimas Bayu Nugroho <193190491+DimasVSuper@users.noreply.github.com>
2025-03-05 16:04:17 +0700

    Commit ManipulasiString change

--- ManipulasiString.php
+++ ManipulasiString.php
@@ -1,26 +1,36 @@
 <?php
 
-$name = "Eko Kurniawan Khannedy";
+$name = "Dimas Bayu Nugroho";
 
-echo "Name : " . $name . PHP_EOL;
-echo "Value : " . 100 . PHP_EOL;
+// Menggabungkan string menggunakan operator titik (.)
+echo "Name : " . $name . PHP_EOL; // Menampilkan "Name : Dimas Bayu Nugroho"
+echo "Value : " . 100 . PHP_EOL; // Menampilkan "Value : 100"
 
+// Konversi tipe data ke string
 $valueString = (string)100;
-var_dump($valueString);
+var_dump($valueString); // Menampilkan tipe data dan nilai dari $valueString, yaitu string(3) "100"
 
+// Konversi tipe data ke integer
 $valueInt = (int)"100";
-var_dump($valueInt);
+var_dump($valueInt); // Menampilkan tipe data dan nilai dari $valueInt, yaitu int(100)
 
+// Konversi tipe data ke float
 $valueFloat = (float)"1.01";
-var_dump($valueFloat);
+var_dump($valueFloat); // Menampilkan tipe data dan nilai dari $valueFloat, yaitu float(1.01)
 
-$name = "Eko";
-echo $name[0] . PHP_EOL;
-echo $name[1] . PHP_EOL;
-echo $name[2] . PHP_EOL;
+$name = "Dimas";
+// Mengakses karakter dalam string menggunakan indeks
+echo $name[0] . PHP_EOL; // Menampilkan karakter pertama dari string $name, yaitu "E"
+echo $name[1] . PHP_EOL; // Menampilkan karakter kedua dari string $name, yaitu "k"
+echo $name[2] . PHP_EOL; // Menampilkan karakter ketiga dari string $name, yaitu "o"
 
-echo "Hello " . $name . ", Selamat Belajar PHP" . PHP_EOL;
-echo "Hello $name, Selamat Belajar PHP" . PHP_EOL;
+// Menggabungkan string dengan variabel menggunakan operator titik (.)
+echo "Hello " . $name . ", Selamat Belajar PHP" . PHP_EOL; // Menampilkan "Hello Eko, Selamat Belajar PHP"
 
-$var = "Eko";
-echo "This is {$var}s" . PHP_EOL;
+// Menggabungkan string dengan variabel menggunakan interpolasi
+echo "Hello $name, Selamat Belajar PHP" . PHP_EOL; // Menampilkan "Hello Eko, Selamat Belajar PHP"
+
+// Menggunakan kurung kurawal untuk mengakses variabel dalam string
+$var = "Dimas";
+echo "This is {$var}s" . PHP_EOL; // Menampilkan "This is Ekos"
+?>
